387f457b7c19923b27b01d63a0e3052fdb9c3139
Platane <brongniart.arthur@gmail.com>
2015-03-27 15:08:05 +0100

    clean up

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 
-var gauss
-;(function(){
-
-var k = Math.sqrt( 2*Math.PI )
-gauss = function( cx, cy, tau, x, y ){
+var gauss = function( cx, cy, tau, x, y ){
 
     var xx = cx - x
     var yy = cy - y
@@ -18,9 +14,18 @@ gauss = function( cx, cy, tau, x, y ){
         return Math.exp( -0.5 * d  )
 }
 
-})()
+var valueAt=  function(x,y){
+    var v = 0
+    for( var i = blob.length; i--; ){
+        var u = blob[ i ]
+        v += gauss( u.x, u.y, u.tau, x, y)
+    }
+    return v
+}
 
-
+var thresholdValueAt = function(x,y){
+    return valueAt( x,y ) < 0.5  ? 1 : 0
+}
 
 
 var blob = [
@@ -32,101 +37,90 @@ var blob = [
 
 
 
-var valueAt=  function(x,y){
-    var v = 0
-    for( var i = blob.length; i--; ){
-        var u = blob[ i ]
-        v += gauss( u.x, u.y, u.tau, x, y)
+
+
+
+var draw
+;(function(){
+    var canvas = document.createElement( 'canvas' )
+    document.body.appendChild( canvas )
+    var ctx = canvas.getContext( '2d' )
+    var draw = function( maxW, maxH, fnValue ){
+
+        var w = canvas.width = 300
+        var h = canvas.height = 300
+
+
+        var r = Math.max( maxW/w , maxH/h )
+
+        var imgData = ctx.getImageData( 0, 0, w, h )
+        var arr = imgData.data
+
+
+        var x, y, e, k
+        for(var i = w; i-- ;)
+        for(var j = h; j-- ;)
+        {
+            x = i * r
+            y = j * r
+
+            k = (i*w + j)<<2
+
+            e = fnValue( x, y )
+
+            arr[ k ] = arr[ k+1 ] = arr[ k+2 ] = 0|(e * 255)
+            arr[ k+3 ] = 255
+        }
+
+        ctx.putImageData( imgData, 0, 0 )
     }
-    return v
-}
-
-
-
-var canvas = document.createElement( 'canvas' )
-document.body.appendChild( canvas )
-var ctx = canvas.getContext( '2d' )
-var draw = function( maxW, maxH, fnValue ){
-
-    var w = canvas.width = 300
-    var h = canvas.height = 300
-
-    canvas.style.width = window.innerWidth+'px'
-    canvas.style.height = window.innerHeight+'px'
-
-    var r = Math.max( maxW/w , maxH/h )
-
-    var imgData = ctx.getImageData( 0, 0, w, h )
-    var arr = imgData.data
-
-
-    var x, y, e, k
-    for(var i = w; i-- ;)
-    for(var j = h; j-- ;)
-    {
-        x = i * r
-        y = j * r
-
-        k = (i*w + j)<<2
-
-        e = fnValue( x, y )
-
-        arr[ k ] = arr[ k+1 ] = arr[ k+2 ] = 0|(e * 255)
-        arr[ k+3 ] = 255
-    }
-
-    ctx.putImageData( imgData, 0, 0 )
-}
-
-var valueFn = function(x,y){
-    return valueAt( x,y ) < 0.5  ? 1 : 0
-}
+})()
 
 
 
 var cycle
 ;(function(){
 
-var sin = function( c, A, w, phy, t ){
-    return c + A * Math.sin( w * t + phy )
-}
+    var sin = function( c, A, w, phy, t ){
+        return c + A * Math.sin( w * t + phy )
+    }
 
-var rand = function( a, b ){
-    return Math.random() * ( b-a ) + a
-}
+    var rand = function( a, b ){
+        return Math.random() * ( b-a ) + a
+    }
 
-var posFn = blob.map( function(){
+    var posFn = blob.map( function(){
 
-    var fx = sin.bind( null, rand( 50, 150 ), rand( 10, 80 ), rand( 0.01, 0.06 ), rand(0, 6.28)  )
-    var fy = sin.bind( null, rand( 50, 150 ), rand( 10, 80 ), rand( 0.01, 0.06 ), rand(0, 6.28)  )
+        var fx = sin.bind( null, rand( 50, 150 ), rand( 10, 80 ), rand( 0.01, 0.06 ), rand(0, 6.28)  )
+        var fy = sin.bind( null, rand( 50, 150 ), rand( 10, 80 ), rand( 0.01, 0.06 ), rand(0, 6.28)  )
 
-    return function( t ){
-        return {
-            x: fx( t ),
-            y: fy( t )
+        return function( t ){
+            return {
+                x: fx( t ),
+                y: fy( t )
+            }
         }
+    })
+
+
+
+    var t = 0
+    cycle = function(){
+
+        t++
+
+        for( var i = blob.length; i--; ){
+            var p = posFn[ i ]( t )
+            blob[ i ].x = p.x
+            blob[ i ].y = p.y
+        }
+
+        stats.begin()
+        draw( 200, 200, thresholdValueAt )
+        stats.end()
+
+        window.requestAnimationFrame( cycle )
     }
-})
-
-
-
-var t = 0
-cycle = function(){
-
-    t++
-
-    for( var i = blob.length; i--; ){
-        var p = posFn[ i ]( t )
-        blob[ i ].x = p.x
-        blob[ i ].y = p.y
-    }
-
-    stats.begin()
-    draw( 200, 200, valueFn )
-    stats.end()
-
-    window.requestAnimationFrame( cycle )
-}
 
 })()
 
@@ -170,5 +164,7 @@ window.onload = function(){
         }
     }
 
+
+    // start loop
     cycle()
 }
