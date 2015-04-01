a3025189e630f40f57020fe18b36c10f2a260b98
Platane <brongniart.arthur@gmail.com>
2015-04-01 18:11:59 +0200

    load file from disk

--- app.js
+++ app.js
@@ -1,76 +1,87 @@
 import {AnalyzerPlayer} from './analyzerPlayer'
 
 
-// xhr request the sound file as buffer
-let loadSound = function( url ){
-
-    let request = new XMLHttpRequest()
-    request.open( 'GET', url )
-    request.responseType = 'arraybuffer'
-
-    return new Promise(function( resolve, reject ){
-
-        request.onload = () => resolve( request.response )
-
-        request.send()
-    })
-}
-
-
-
 // instanciate the soundPlayer
 let soundPlayer = new AnalyzerPlayer()
 
-loadSound( 'sample.mp3' )
 
-.then( soundPlayer.bindBuffer.bind( soundPlayer ) )
-.then( () => console.log('loaded, playing') )
-.then( soundPlayer.play.bind( soundPlayer ) )
+// create the file input
+;(function(){
+
+    // TODO filter
 
 
-// error handling
-.then( null, err => console.log( err.stack ) )
+    let readFile = function( file ){
+
+        let reader = new FileReader()
+
+        return new Promise( function( resolve, reject ){
+
+            reader.onload = () => resolve( reader.result )
+
+            reader.onError = reject
+
+            reader.readAsArrayBuffer( file )
+        })
+    }
+
+    let processFile = ( file ) => readFile( file )
+            .then( soundPlayer.bindBuffer.bind( soundPlayer ) )
+            .then( () => console.log('playing  ..') )
+            .then( soundPlayer.play.bind( soundPlayer ) )
+            .then( cycle )
+
+            .then( null, err => console.log( err.stack ) )
+
+
+    let dom = document.createElement('input')
+    dom.setAttribute('type', 'file')
+    document.body.appendChild( dom )
+    dom.addEventListener('change', (event) => processFile( event.target.files[0] ) )
+
+})()
+
 
 
 var drawF = (function(){
-var canvas = document.createElement( 'canvas' )
-document.body.appendChild( canvas )
-var ctx = canvas.getContext( '2d' )
-return function( beat ){
-    var w = canvas.width = 1024
-    var h = canvas.height = 512
+    var canvas = document.createElement( 'canvas' )
+    document.body.appendChild( canvas )
+    var ctx = canvas.getContext( '2d' )
+    return function( beat ){
+        var w = canvas.width = 1024
+        var h = canvas.height = 512
 
-    ctx.clearRect( 0, 0, w, h )
-    ctx.fillStyle = '#000000'
-    ctx.globalAlpha = 1
+        ctx.clearRect( 0, 0, w, h )
+        ctx.fillStyle = '#000000'
+        ctx.globalAlpha = 1
 
 
-    var arr = beat.avgFreq
-    var l = Math.min( arr.length, 64 )
+        var arr = beat.avgFreq
+        var l = Math.min( arr.length, 64 )
 
-    var maxVal = 255
-    var minVal = 0
+        var maxVal = 255
+        var minVal = 0
 
-    for( var k = 0; k<l ; k++ ){
+        for( var k = 0; k<l ; k++ ){
 
-        ctx.beginPath()
-        ctx.rect( k/l*w+1, (1-arr[ k ]/maxVal)*h, 1/l*w-2, arr[ k ]/maxVal*h )
-        ctx.fill()
+            ctx.beginPath()
+            ctx.rect( k/l*w+1, (1-arr[ k ]/maxVal)*h, 1/l*w-2, arr[ k ]/maxVal*h )
+            ctx.fill()
+        }
+
+
+        ctx.globalAlpha = 0.7
+        var arr = beat.smoothInstantFreq
+
+        for( var k = 0; k<l ; k++ ){
+
+
+            ctx.fillStyle = k == 5 ? '#1237AE' : '#823234'
+            ctx.beginPath()
+            ctx.rect( k/l*w+3, (1-arr[ k ]/maxVal)*h, 1/l*w-6, arr[ k ]/maxVal*h )
+            ctx.fill()
+        }
     }
-
-
-    ctx.globalAlpha = 0.7
-    var arr = beat.smoothInstantFreq
-
-    for( var k = 0; k<l ; k++ ){
-
-
-        ctx.fillStyle = k == 5 ? '#1237AE' : '#823234'
-        ctx.beginPath()
-        ctx.rect( k/l*w+3, (1-arr[ k ]/maxVal)*h, 1/l*w-6, arr[ k ]/maxVal*h )
-        ctx.fill()
-    }
-}
 })()
 
 
